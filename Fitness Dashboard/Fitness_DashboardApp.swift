bb2ec4fcd989da401090000e4028f736e2103101
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-07 20:31:26 +0530

    Initial Commit

---
+++ Fitness Dashboard/Fitness_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_DashboardApp.swift
+//  Fitness Dashboard
+//
+//  Created by Mahesh Prasad on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
